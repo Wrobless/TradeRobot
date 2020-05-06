b5b9fff4fea861e1136b87a028fd3c65a0719eaa
wrobless <awroblewski91@gmail.com>
2020-05-06 15:56:44 +0200

    added compile script

---
+++ compile.bat
@@ -0,0 +1,8 @@
+c:
+cd C:\Program Files (x86)\MetaTrader 4 IC Markets
+metaeditor.exe /compile:"D:\OneDrive\Meta Trader\MQ4 Repo\TradeRobot\Test.mq4"
+d:
+cd D:\OneDrive\Meta Trader\MQ4 Repo\TradeRobot
+copy Test.ex4 C:\Users\Adam\AppData\Roaming\MetaQuotes\Terminal\1DAFD9A7C67DC84FE37EAA1FC1E5CF75\MQL4\Experts
+copy Test.mq4 C:\Users\Adam\AppData\Roaming\MetaQuotes\Terminal\1DAFD9A7C67DC84FE37EAA1FC1E5CF75\MQL4\Experts
+PAUSE
